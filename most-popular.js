241ebaec4af0e80d6447f7aca7ebde10d08c1390
David Lee <davidlee980804@gmail.com>
2020-11-10 16:23:18 -0800

    Initalized the repository with all the files

---
+++ most-popular.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/* This function takes in a string and returns the most common letter */
+
+function mostPopular(word) {
+  let ltrCount = {};
+  let maxCount = 0;
+  let mostCommonLtr;
+
+  for (let letter of word) {
+    // Add one to the previous count of the letter, or if it's the 
+    // first occurrence of the letter, set the letter count to 1 
+    ltrCount[letter] = ltrCount[letter] + 1 || 1;
+
+    if (ltrCount[letter] > maxCount) {
+      maxCount = ltrCount[letter];
+      mostCommonLtr = letter;
+    }
+  }
+
+  return mostCommonLtr;
+}
---
+++ word-scores.js
@@ -0,0 +1,47 @@
+function wordScores(words) {
+  let scores = [];
+
+  for (let word of words) {
+    let wordScore = 0;
+
+    for (let letter of word) {
+      if (!(letter.toUpperCase() in scoreKey)) {
+        wordScore = null;
+        break;
+      }
+
+      wordScore += scoreKey[letter.toUpperCase()];
+    }
+    scores.push(wordScore);
+  }
+  return scores;
+}
+
+let scoreKey = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10
+};
